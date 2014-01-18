7e9f9d8c478a913a9874d61bc1dbcea1be1b7527
Kapil Thangavelu <kapilt@gmail.com>
2014-01-18 09:37:48 -0500

    artistic sketch mode

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='juju-docean',
+      version="0.0.3",
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Programming Language :: Python',
+          'Operating System :: OS Independent'],
+      author='Kapil Thangavelu',
+      author_email='kapil.foss@gmail.com',
+      description="Digital Ocean integration with juju",
+      long_description=open("README.rst").read(),
+      url='http://github/kapilt/juju-docean',
+      license='BSD',
+      packages=find_packages(),
+      install_requires=["jujuclient", "PyYAML", "dop"],
+      entry_points={
+          "console_scripts": [
+              'juju-docean = juju_docean.cli:main']},
+      )
