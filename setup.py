957a63271726681866d8be1ae6f5fe3b26231d16
Kapil Thangavelu <kapilt@gmail.com>
2014-02-15 19:10:16 -0500

    version incr

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='juju-docean',
-      version="0.0.8",
+      version="0.0.9",
       classifiers=[
           'Intended Audience :: Developers',
           'Programming Language :: Python',
